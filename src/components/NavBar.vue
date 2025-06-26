4671a9ee18f9e03c1aab353e803dd386223d5d7d
Eugene Peralta <157940784+Shishwami@users.noreply.github.com>
2025-06-26 09:18:01 +0800

    Create NavBar.vue
    Basic layout with basic styling

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="#home">Jurghen</a></li>
+        </ul>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="contact">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #333;
+    color: white;
+}
+ul {
+    list-style: none;
+    display: flex;
+    gap: 20px;
+}
+li {
+    cursor: pointer;
+}
+a {
+    color: white;
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: underline;
+}
+</style>
