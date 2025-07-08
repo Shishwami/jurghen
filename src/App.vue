3142b5ab9d54a13881109f76ede19509658b9535
Eugene Peralta <157940784+Shishwami@users.noreply.github.com>
2025-07-08 10:27:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import HeroPage from './components/HeroPage.vue';
+import SkillSection from './components/SkillSection.vue';
+import Projects from './components/Projects.vue';
+import Certificates from './components/Certificates.vue';
+import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <div>
-    <NavBar/>
+    <NavBar />
+    <HeroPage />
+    <SkillSection />
+    <Projects />
+    <Certificates />
+    <Contact />
+    <Footer />
   </div>
 </template>
-
